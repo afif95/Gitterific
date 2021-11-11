11af38374c2456c71936156e7a6024a5cbd39db0
RoxaneTissier <roxane.tissier95@gmail.com>
2021-11-11 13:10:48 -0500

    test

---
+++ target/scala-2.13/classes/controllers/javascript/JavaScriptReverseRoutes.scala
@@ -0,0 +1,134 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:conf/routes
+
+import play.api.routing.JavaScriptReverseRoute
+
+
+import _root_.controllers.Assets.Asset
+import _root_.play.libs.F
+
+// @LINE:6
+package controllers.javascript {
+
+  // @LINE:14
+  class ReverseHomeController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:17
+    def time: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.HomeController.time",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "time"})
+        }
+      """
+    )
+  
+    // @LINE:14
+    def index: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.HomeController.index",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + """"})
+        }
+      """
+    )
+  
+    // @LINE:16
+    def tutorial: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.HomeController.tutorial",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "tutorial"})
+        }
+      """
+    )
+  
+    // @LINE:15
+    def explore: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.HomeController.explore",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "explore"})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:21
+  class ReverseAssets(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:21
+    def versioned: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Assets.versioned",
+      """
+        function(file1) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "assets/" + (""" + implicitly[play.api.mvc.PathBindable[Asset]].javascriptUnbind + """)("file", file1)})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:6
+  class ReverseApiController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:10
+    def home: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.ApiController.home",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "home"})
+        }
+      """
+    )
+  
+    // @LINE:6
+    def index1: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.ApiController.index1",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "index"})
+        }
+      """
+    )
+  
+    // @LINE:8
+    def searchQuery: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.ApiController.searchQuery",
+      """
+        function(key0) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "searchQuery/" + encodeURIComponent((""" + implicitly[play.api.mvc.PathBindable[String]].javascriptUnbind + """)("key", key0))})
+        }
+      """
+    )
+  
+    // @LINE:12
+    def save: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.ApiController.save",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "home"})
+        }
+      """
+    )
+  
+  }
+
+
+}
