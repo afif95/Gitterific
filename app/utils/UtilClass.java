98b1a20cc40ae74cc62c6414f51dc76cf1961ba4
RimshaHAfzal <rimsha.afzal.ra@gmail.com>
2021-11-20 12:12:11 -0500

    Comments and a few fixes

--- app/utils/UtilClass.java
+++ app/utils/UtilClass.java
@@ -18,25 +18,81 @@ import play.libs.ws.WSResponse;
 
 public class UtilClass {
 	
+	/**This method is used for deserialization. 
+	 * It converts the JsonNode data into a list of 
+	 * objects of <code> Repository</code>.
+	 * 
+	 * 
+	 * @author Amrit Singh
+	 * @param node JsonNode extracted from an HTTP request 
+	 * @return list of repositories
+	 */
 	public List<Repository> JSONtoRepoList(JsonNode node) {
 		return StreamSupport.stream( node.get("items").spliterator(), false)
                 .map(sObj -> Json.fromJson(sObj, Repository.class))
                 .collect(Collectors.toList());
 	}
 	
+	
+	/**This method is used for deserialization. 
+	 * It converts the JsonNode data into an objects 
+	 * of <code> PublicOwnerInfo</code>.
+	 * 
+	 * 
+	 * @author Amrit Singh/Rimsha Afzal
+	 * @param node JsonNode extracted from an HTTP request 
+	 * @return an object of PublicOwnerInfo
+	 */
+	
 	public PublicOwnerInfo getPublicOwnerInfo(JsonNode node) {	 
     	return Json.fromJson(node.get("owner"), PublicOwnerInfo.class);
     }
 	
 	
+	/**This method is used for deserialization. 
+	 * It converts the JsonNode data into an object
+	 * of <code> PublicRepositoryInfo</code>.
+	 * 
+	 * 
+	 * @author Amrit Singh/Roxane Tissier
+	 * @param node JsonNode extracted from an HTTP request 
+	 * @return an object of PublicRepositoryInfo
+	 */
+	
 	public PublicRepositoryInfo getPublicRepositoryInfo(JsonNode node) {
     	return  Json.fromJson(node, PublicRepositoryInfo.class);    
 	}
 	
+	
+	/**This method is used for deserialization. 
+	 * It converts the JsonNode data into a list of names of
+	 * the repositories.
+	 * 
+	 * @author Amrit Singh/Rimsha Afzal
+	 * @param node JsonNode extracted from an HTTP request 
+	 * @return a list of repositories' name
+	 */
+	
 	public List<String> getOwnerRepos(JsonNode node) {
-    	return node.findValues("name").stream().map(JsonNode::asText)
-    			.collect(Collectors.toList());
+		return node.findValues("full_name").stream().map(a->{
+			String b = a.asText();
+			return b.split("/")[1];})
+    		.collect(Collectors.toList());
 	}
+	
+	/**This method is used for deserialization and storing users. 
+	 * It converts the JsonNode data into an object
+	 * of <code> PublicOwnerInfo</code> and maintains a record of the users
+	 * that have made a search.
+	 * 
+	 * 
+	 * @author Amrit Singh/Rimsha Afzal
+	 * @param node JsonNode extracted from an HTTP request
+	 * @ownerMap the map of user login and his object
+	 * @searchKey owner's login
+	 * @return HashMap <String, PublicOwnerInfo> a data structure storing user and his profile
+	 */
+	
 	
 	public HashMap <String, PublicOwnerInfo> insertKeyInOwnerMap(JsonNode node, HashMap <String, PublicOwnerInfo> ownerMap, String searchKey) {
 		
@@ -48,6 +104,20 @@ public class UtilClass {
     	return ownerMap;
     
 	}
+	
+	/**This method is used for deserialization and storing searches. 
+	 * It converts the JsonNode data into an object
+	 * of <code> PublicOwnerInfo</code> and maintains a record of the searches
+	 * that a user has made.
+	 * 
+	 * 
+	 * @author Amrit Singh/Rimsha Afzal
+	 * @param node JsonNode extracted from an HTTP request
+	 * @ownerMap the map of users and his search results
+	 * @searchKey the search the user made 
+	 * @return HashMap <String, PublicOwnerInfo> a data structure storing user and his searches
+	 */
+	
 	
 	public HashMap <String, List<Repository>> addRepoToUserMap(String s, HashMap <String, List<Repository>> user_searches, List<Repository> list) {
 
